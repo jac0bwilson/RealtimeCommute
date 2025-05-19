772f48df21e9926fac53c047aabd6c8f94208f27
Jacob Wilson <jacobswilsonglos@gmail.com>
2025-05-19 20:26:17 +0100

    Fix incorrect field serialisation

--- composeApp/src/commonMain/kotlin/uk/jacobw/commute/data/model/Station.kt
+++ composeApp/src/commonMain/kotlin/uk/jacobw/commute/data/model/Station.kt
@@ -1,11 +1,12 @@
 package uk.jacobw.commute.data.model
 
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 import uk.jacobw.commute.data.database.StationEntity
 
 @Serializable
 data class Station(
-    val name: String,
+    @SerialName("stationName") val name: String,
     val crsCode: String,
 ) {
     fun toStationEntity(): StationEntity =
