d88e977126a577300159bde616b70bf7b67448b0
Jacob Wilson <jacobswilsonglos@gmail.com>
2025-05-25 13:17:49 +0100

    Fix text wrapping issue

--- composeApp/src/commonMain/kotlin/uk/jacobw/commute/feature/service/ServiceLayout.kt
+++ composeApp/src/commonMain/kotlin/uk/jacobw/commute/feature/service/ServiceLayout.kt
@@ -4,7 +4,6 @@ import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
@@ -21,6 +20,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import org.jetbrains.compose.resources.stringResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
@@ -143,7 +143,8 @@ private fun LocationItem(location: Location) {
     Row(
         modifier =
             Modifier
-                .padding(horizontal = 16.dp, vertical = 4.dp),
+                .padding(horizontal = 16.dp, vertical = 4.dp)
+                .fillMaxWidth(),
         horizontalArrangement = Arrangement.spacedBy(4.dp),
         verticalAlignment = Alignment.CenterVertically,
     ) {
@@ -156,9 +157,11 @@ private fun LocationItem(location: Location) {
                 text = correctionString(plannedTime, actualTime),
             )
         }
-        Text(location.description)
-
-        Spacer(modifier = Modifier.weight(1f))
+        Text(
+            text = location.description,
+            modifier = Modifier.weight(1f),
+            overflow = TextOverflow.MiddleEllipsis,
+        )
 
         PlatformIndicator(location.platformState)
     }
